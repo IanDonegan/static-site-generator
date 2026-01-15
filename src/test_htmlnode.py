9bb70833cf5e8c24992b33be76e71240d75154d8
Ian Donegan <IanDonegan@IanDonegan.com>
2026-01-15 16:16:01 -0500

    Added HTMLNode with tests.

---
+++ src/test_htmlnode.py
@@ -0,0 +1,14 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode("This is a tag", value="value", props={"href":"https://www.google.com","target":"_blank"})
+        self.assertEqual(node.props_to_html(),'href="https://www.google.com" target="_blank"')
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
